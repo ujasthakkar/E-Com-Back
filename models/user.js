9ff8b83033dc35d498a0865be1469b6c61a7ccaf
ujasthakkar <62230701+ujasthakkar@users.noreply.github.com>
2021-03-31 12:56:11 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,62 @@
+var mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+  
+
+
+  var userSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+
+    lastname: {
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    userinfo: {
+        type: String,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true
+
+    },
+    role: {
+        type: Number,
+        default: 0
+
+    },
+
+    purchases: {
+        type: Array,
+        default: []
+    },
+    
+  },
+  {timestamps: true}
+  );
+
+  userSchema.methods = {
+
+    authenticate: function(plainpassword) {
+        return plainpassword === this.password
+    }
+}
+  
+
+  module.exports = mongoose.model("User",userSchema)
