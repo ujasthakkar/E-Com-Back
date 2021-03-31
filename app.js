7393d3968d40d1bdfe328f4a20859175a7b9ef91
ujasthakkar <62230701+ujasthakkar@users.noreply.github.com>
2021-03-31 12:16:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const User = require("./models/user");
+
+
+
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() =>{
+    console.log("DB CONNECTED")
+});
+
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 4001;
+
+
+
+
+//Starting a server 
+app.listen(port, () =>{
+    console.log(`app is running at ${port}`);
+})
